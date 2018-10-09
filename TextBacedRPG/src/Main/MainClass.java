81e908daba6a8fb6114933faeb8d309aa1529f2a
id129001 <34245738+id129001@users.noreply.github.com>
2018-10-09 09:51:08 -0500

    added ints and strings

---
+++ TextBacedRPG/src/Main/MainClass.java
@@ -0,0 +1,25 @@
+package Main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+	    Random rand = new Random();
+	    String playerName;
+	    int playerhp;
+	     int maxhp;
+	     int maxmana;
+	     int mana;
+	     int playermeleedmg;
+	     int xp;
+	     int enemyhp;
+	     int enemymeleedmg;
+	     int Level;
+	     String charclass;
+	}
+
+}
