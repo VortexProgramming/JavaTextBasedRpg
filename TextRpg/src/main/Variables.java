e1f9a3c75f6265f1570819e442fb0548a5a08ffb
Jordan Elliott <37377660+YourTechie@users.noreply.github.com>
2018-10-10 08:43:06 -0500

    Started

--- TextRpg/src/main/Variables.java
+++ TextRpg/src/main/Variables.java
@@ -1,10 +1,14 @@
 package main;
 
-public class Variables {
+public class Variables 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		String playerName;
+		int playerHP;
+		
+		
 	}
 
 }
