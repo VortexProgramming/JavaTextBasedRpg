835decb3059d2c227e35654bc6779000736accb1
id129001 <34245738+id129001@users.noreply.github.com>
2018-10-12 09:48:20 -0500

    Stuff

--- TextBacedRPG/src/Main/Cave.java
+++ TextBacedRPG/src/Main/Cave.java
@@ -1,10 +1,31 @@
 package Main;
 
+import java.sql.Date;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Cave {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int die1;
+		int spooderHP;
+		int cavespooderHP;
+		int spooderDMG;
+		int cavespooderDMG = 0;
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+	    Date die = new Date(cavespooderDMG);
+	    die1 = (int)(Math.random()*6) + 1;
+		System.out.println("You've entered the cave");
+	}
+		public static void enemySpooder()
+		{
+			
+		}
+		public static void enemyCaveSpooder()
+		{
+		
 	}
 
 }
