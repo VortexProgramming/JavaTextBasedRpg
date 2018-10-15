589df3edd7e9ed29a5fca9ed3e77bd165bac7ac7
id129001 <34245738+id129001@users.noreply.github.com>
2018-10-15 08:43:28 -0500

    stuff

--- TextBacedRPG/src/Main/DragonsLair.java
+++ TextBacedRPG/src/Main/DragonsLair.java
@@ -4,7 +4,9 @@ public class DragonsLair {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int dagonHP = 100; 
+		int dagonDMG;
+		System.out.println("You've entered the Dragon's Lair");
 	}
 
 }
